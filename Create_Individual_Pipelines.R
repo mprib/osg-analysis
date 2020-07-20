51d11f8dafdfb4416cd54dd841359fa0b66d4124
mprib <prible@gmail.com>
2020-07-20 18:58:56 -0500

    pipeline creation largely working

--- Create_Individual_Pipelines.R
+++ Create_Individual_Pipelines.R
@@ -3,22 +3,34 @@ library(here)
 
 df_Subject_Metrics <- readxl::read_excel(here("SubjectAnthropometrics.xlsx"))
 
-txt_pipeline <- read_lines(here("osg-pipeline-template.v3s"))
+txt_pipeline_template <- read_lines(here("osg-pipeline-template.v3s"))
 
-for (subject in df_Subject_Metrics$SubjectID) {
-  print(subject)  
-  
-}
-
-subject_default <- "2015043"
+# These are the values in the file above that will be overwritten
+subject_default <- "2014001"
 mass_default <- "67"
 height_default <-"172.5"
 
-subject <- "2015042"
-mass <-  "100"      
-height <- "1.5"     
 
-txt_pipeline <-  txt_pipeline %>% str_replace_all(subject_default, subject)
-txt_pipeline <-  txt_pipeline %>% str_replace_all(mass_default, mass)
-txt_pipeline <-  txt_pipeline %>% str_replace_all(height_default, height)
+for (subjectID in df_Subject_Metrics$SubjectID) {
+  
+  print(subject)  
+  
+  subject <- as.character(subjectID)
+  
+  mass <- df_Subject_Metrics %>% 
+          filter(SubjectID == subjectID) %>% 
+          select(Mass) %>% as.character()
+  
+  height <- df_Subject_Metrics %>% 
+            filter(SubjectID == subjectID) %>% 
+            select(Height)%>% as.character()
+  
+  txt_pipeline <-  txt_pipeline_template %>% str_replace_all(subject_default, subject)
+  txt_pipeline <-  txt_pipeline %>% str_replace_all(mass_default, mass)
+  txt_pipeline <-  txt_pipeline %>% str_replace_all(height_default, height)
+  
+  write_lines(txt_pipeline, here("pipelines", paste0(subject, ".v3s")))
+  
+}
+
 
